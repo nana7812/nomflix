3e499b0d089a31b38967402414cdf987dcc44985
nana7812 <zcjswkz0812@naver.com>
2021-02-03 22:16:01 +0900

    Router

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default () => (
+  <header>
+    <ul>
+      <li>
+        <a href="/">Movies</a>
+      </li>
+      <li>
+        <a href="/tv">Tv</a>
+      </li>
+      <li>
+        <a href="/search">Serarch</a>
+      </li>
+    </ul>
+  </header>
+);
