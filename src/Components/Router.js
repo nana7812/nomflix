d5e45169b5217f15687a9119ab957e6268ebff0c
nana7812 <zcjswkz0812@naver.com>
2021-02-04 10:49:16 +0900

    #3 Styles_CSS

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,14 +8,15 @@ import {
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 export default () => (
   <Router>
+    <Header />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
       {/* <Route path="/tv/popular" render={() => <h1>Popular</h1>} /> */}
-
       <Route path="/search" exact component={Search} />
       <Redirect from="*" to="/" />
     </Switch>
