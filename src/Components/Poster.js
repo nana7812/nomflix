da91f972e216285748925cd3a91530a257355763
nana7812 <zcjswkz0812@naver.com>
2021-02-09 18:57:33 +0900

    PRESENTERS

---
+++ src/Components/Poster.js
@@ -0,0 +1,86 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const Container = styled.div`
+  font-size: 12px;
+`;
+
+const Image = styled.div`
+  background-image: url(${(props) => props.bgUrl});
+  height: 180px;
+  background-size: cover;
+  border-radius: 4px;
+  background-position: center center;
+  transition: opacity 0.1s linear;
+`;
+
+const Rating = styled.span`
+  bottom: 5px;
+  right: 5px;
+  position: absolute;
+  opacity: 0;
+  transition: opacity 0.1s linear;
+`;
+
+const ImageContainer = styled.div`
+  margin-bottom: 5px;
+  position: relative;
+  &:hover {
+    ${Image} {
+      opacity: 0.3;
+    }
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
+
+const Title = styled.span`
+  display: block;
+
+  margin-bottom: 3px;
+`;
+
+const Year = styled.span`
+  font-size: 10px;
+  color: rgba(255, 255, 255, 0.7);
+`;
+
+const Poster = ({ id, imgUrl, title, rating, year, isMovie = false }) => (
+  <Link to={isMovie ? `/movie/${id}` : `/show${id}`}>
+    <Container>
+      <ImageContainer>
+        <Image
+          bgUrl={
+            imgUrl
+              ? `https://image.tmdb.org/t/p/w300${imgUrl}`
+              : require("../assets/noPosterSmall.png").default
+          }
+        />
+        <Rating>
+          <span role="img" aria-label="rating">
+            ⭐️
+          </span>{" "}
+          {rating}/10
+        </Rating>
+      </ImageContainer>
+      <Title>
+        {title.length > 18 ? `${title.substring(0, 15)}...` : title}
+      </Title>
+      <Year>{year}</Year>
+    </Container>
+  </Link>
+);
+
+Poster.propTypes = {
+  id: PropTypes.number.isRequired,
+  imgUrl: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+  year: PropTypes.string,
+  isMovie: PropTypes.bool.isRequired,
+};
+
+export default Poster;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,6 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "Components/Loader";
+import Section from "Components/Section";
+import Message from "Components/Message";
+import Poster from "Components/Poster";
+
+const Container = styled.div`
+  padding: 0px 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 28px;
+  width: 100%;
+`;
 
 const SearchPresenter = ({
   movieResults,
@@ -9,7 +28,62 @@ const SearchPresenter = ({
   loading,
   error,
   handleSubmit,
-}) => null;
+  updateTerm,
+}) => (
+  <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder="Search Movies or Tv show..."
+        value={searchTerm}
+        onChange={updateTerm}
+      />
+    </Form>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        {movieResults && movieResults.length > 0 && (
+          <Section title="Movie Results">
+            {movieResults.map((movie) => (
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                imgUrl={movie.poster_path}
+                title={movie.original_title}
+                rating={movie.vote_average}
+                year={movie.release_date && movie.release_date.substring(0, 4)}
+                isMovie={true}
+              />
+            ))}
+          </Section>
+        )}
+        {tvResults && tvResults.length > 0 && (
+          <Section title="TV Results">
+            {tvResults.map((show) => (
+              <Poster
+                key={show.id}
+                id={show.id}
+                imgUrl={show.poster_path}
+                title={show.original_name}
+                rating={show.first_air_date}
+                year={
+                  show.first_air_date && show.first_air_date.substring(0, 4)
+                }
+              />
+            ))}
+          </Section>
+        )}
+        {error && <Message color="#e74c3c" text={error} />}
+        {tvResults &&
+          movieResults &&
+          tvResults.length === 0 &&
+          movieResults.length === 0 && (
+            <Message text="Nothing found" color="#95a5a6" />
+          )}
+      </>
+    )}
+  </Container>
+);
 
 SearchPresenter.propTypes = {
   movieResults: PropTypes.array,
@@ -18,6 +92,7 @@ SearchPresenter.propTypes = {
   loading: PropTypes.bool.isRequired,
   error: PropTypes.string,
   handleSubmit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired,
 };
 
 export default SearchPresenter;
